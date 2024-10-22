68ff02ecde3fd201a95b6f0d6006eb26f7e6510d
Ryan Kerluke <146582651+ksfryan@users.noreply.github.com>
2024-10-22 00:27:31 +0000

    commit

---
+++ .devcontainer/2-02.py
@@ -0,0 +1,13 @@
+# one road coding exercise
+x = min(a,b,a)
+print(x)
+
+#coding exercise two roads
+x = min(c,b,a,)
+y = min(e,d)
+print(max(y,x))
+
+#coding exercise sort of strangeness
+print(min(x, y, z))
+print(x+y+z-min(x, y, z)-max(x, y, z))
+print(max(x, y, z))
---
+++ .devcontainer/Assignment 2b .py
@@ -0,0 +1,3 @@
+# Finding the area and circumfrence of a circle.
+
+r = (input(Your Diameter))
