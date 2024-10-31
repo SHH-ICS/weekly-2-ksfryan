1c2131bd277abe1de64dc8d7a473739b2cc50cb3
Ryan Kerluke <146582651+ksfryan@users.noreply.github.com>
2024-10-31 14:25:06 +0000

    hi

--- .devcontainer/Assignment 2b .py
+++ .devcontainer/Assignment 2b .py
@@ -1,3 +1,17 @@
 # Finding the area and circumfrence of a circle.
 
-r = (input(Your Diameter))
+r = float(input("Your Radius: "))
+
+if r == 0:
+    print("The radius can't be zero -error")
+elif r < 0:
+    print("The radius can't be in the negatives - error")
+else:
+
+    a = 3.14 * r ** 2
+    print(f"Your area is {a}")
+    
+    c = 3.14 * 2 * r
+    print(f"Your circumference is {c}")
+
+
